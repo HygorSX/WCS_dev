533a59c4a42b615c7e660d3fe0be0448c81f92ac
Hygor <hygorlimatop123@gmail.com>
2024-06-19 16:56:23 -0300

    WCS 2.0

---
+++ WCS_2.0/Controllers/LexmarkController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WCS.Data;
+using WCS.Utilities;
+
+namespace WCS.Controllers
+{
+    public class LexmarkController
+    {
+        public static void EnviarDadosLexmark(Lexmark lexmark)
+        {
+            using (var db = new PrinterMonitoringContext())
+            {
+                try
+                {
+                    db.PrinterMonitoringTESTE.Add(lexmark);
+                    //db.Entry(lexmark).State = System.Data.Entity.EntityState.Added;
+                    db.SaveChanges();
+                    Utils.Log("Dados da impressora enviados com sucesso para o banco de dados.");
+                }
+                catch (DbUpdateException ex)
+                {
+                    Console.WriteLine("Erro ao enviar dados da impressora para o banco de dados: " + ex.Message);
+                    if (ex.InnerException != null)
+                    {
+                        Console.WriteLine("Detalhes da exceção interna: " + ex.InnerException.Message);
+                    }
+                    if (ex.InnerException?.InnerException != null)
+                    {
+                        Console.WriteLine("Mais detalhes da exceção interna: " + ex.InnerException.InnerException.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro inesperado: " + ex.Message);
+                    if (ex.InnerException != null)
+                    {
+                        Console.WriteLine("Detalhes da exceção interna: " + ex.InnerException.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WCS_2.0/Models/Lexmark.cs
@@ -0,0 +1,47 @@
+﻿using SnmpSharpNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using WCS.Utilities;
+
+namespace WCS
+{
+    public class Lexmark : Utils
+    {
+        //public String MacAddress { get; set; } 
+        [Key]
+        public int Id { get; set; }
+        public string Serial { get; set; }
+        public string DeviceManufacturer { get; set; }
+        public string DeviceModel { get; set; }
+        public int QuantidadeImpressaoTotal { get; set; }
+        public int PorcentagemKitManutenção { get; set; }
+        public int PorcentagemBlack { get; set; }
+        private string _printerStatus { get; set; } 
+        public int PorcentagemCyan { get; set; }
+        public int PorcentagemYellow { get; set; }
+        public int PorcentagemMagenta { get; set; }
+        public int PorcentagemFusor { get; set; }
+        public int PorcentagemBelt { get; set; }
+        //public string PrinterStatus { get; set; }
+        public DateTime DataHoraDeBusca { get; set; } = DateTime.Now;
+
+        public string PrinterStatus
+        {
+
+            get
+            {
+                return HexConvert(_printerStatus);
+            }
+
+            set
+            {
+                _printerStatus = value;
+            }
+        }
+    }
+}
